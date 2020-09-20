7f784bdd0d624aeb48df0eca23aa5cb2bc95681f
pvk4498 <70468969+pvk4498@users.noreply.github.com>
2020-09-21 00:09:25 +0530

    Add files via upload

--- garage web app/scripts/search_helper.js
+++ garage web app/scripts/search_helper.js
@@ -1,89 +1,38 @@
-var options = {
-  valueNames: [
-    'name',
-    'price',
-    'address',
-    'city'
-  ],
-  page: 3,
-  pagination: true
-};
-var userList = new List('users', options);
-
-// sort by price-->
-$('.sort').on('keypress', function() {
- sortList();
-});
-
-function sortList() {
-  var searchprice = $(this).val();
-  console.log(searchprice);
-  userList.sort(searchprice, ['price']);
-}
-
-
-function resetList(){
-  userList.search();
-  userList.filter();
-  userList.update();
-  $(".filter-all").prop('checked', true);
-  $('.filter').prop('checked', false);
-  $('.sort').prop('checked', false);
-  $('.search').val('');
-  console.log('Reset Successfully!');
-};
-
-function updateList(){
-  var values_price = $("input[name=price]:checked").val();
-
  
-  userList.update();
-  //console.log('Filtered: ' + values_gender);
-}
+ function get_json(){
+    var json;
+    fetch("https://pvk4498.github.io/webapps/garage%20web%20app/json/data.json")
+        .then(function(resp){
+          return resp.json();
+        })
+        .then(function(data){
+          `
+            ${data.map(function(value) {
+             
+              var options = {
+                valueNames: ['name','address','city','contact'],
+                page: 3,
+                pagination: [{outerWindow: 20}],
+                item: '<div class="card mb-3 box-shadow"><div class="card-body"><ul class="list-unstyled mt-3 mb-4"><div class="card-header"><h4 class="name">F</h4></div><p class="address"></p><p class="city"></p><p class="contact"></p></ul><button type="button" class="btn btn-lg btn-block btn-outline-primary">View Details</button></div></div>'
+                
+              };
+             json = value;
+             var booksList = new List('users', options, json);
+             console.log(json)
 
-$(function(){
-  //updateList();
-  $("input[name=price]").change(updateList);
-
-  userList.on('updated', function (list) {
-    if (list.matchingItems.length > 0) {
-      $('.no-result').hide()
-    } else {
-      $('.no-result').show()
-    }
-  });
-});
-
-
-function get_json(){
-  var name=[];
-fetch("https://pvk4498.github.io/webapps/garage%20web%20app/json/data.json")
-    .then(function(resp){
-      return resp.json();
-    })
-    .then(function(data){
-        
-       document.getElementById("card").innerHTML = `
-        ${data.map(function(value) {
-          return ` 
-          <div class="card border-primary mb-3" style="max-width: 18rem;">
-            <div class="card-header">${value.name}</div>
-              <div class="card-body text-primary">
-                <h5 class="card-title">${value.address}</h5>
-                <p class="card-text">${value.city}</p>
-                <p class="card-text">${value.contact}</p>
-              </div>
-           </div>
-          </div>
+              
+                booksList.on('updated', function (list) {
+                  if (list.matchingItems.length > 0) {
+                    $('.no-result').hide()
+                  } else {
+                    $('.no-result').show()
+                  }
+                });
+            
+            })}
            `
-        }).join("")}
-       `
-    });  
-  
-  }
-
-  get_json();
-
-
-
-
+        });       
+      }
+    
+    get_json();
+     
